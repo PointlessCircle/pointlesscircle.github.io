19ec3909cb91d235ce8cd1bee179b0860892f179
Pointless <150302154+PointlessCircle@users.noreply.github.com>
2024-04-29 18:04:08 -0400

    Small changes

--- script.js
+++ script.js
@@ -31,5 +31,24 @@ function checkCC() {
 }
 
 window.onload = (event) => {
+    document.body.insertAdjacentHTML("beforeend", `
+    <div class="flexrow"
+        style="position: fixed; bottom: 16px; right: 16px; align-items: center; margin:10px; box-shadow:0 0 20px 5px #272727; background: #272727; border-radius: 10px; padding: 10px; display: flex"
+        id="cC">
+        <div style="margin-right: 10px; width: 100px;">
+            <span class="material-symbols-outlined" style="font-size: 100px; user-select: none;">cookie</span>
+        </div>
+        <div style="width: 290px;">
+            <h1 style="margin-top: 0;">mmm... tasty</h1>
+            <p style="text-align: center;">
+                By browsing this site, you agree to our use of cookies.
+            </p>
+            <button style="margin:auto; width: 100%;" onclick="setCookie('cC','true',365)">
+                Accept and close <span class="material-symbols-outlined"
+                    style="vertical-align: top; font-size: 1.2em;  width:1em;">done</span>
+            </button>
+        </div>
+    </div>
+    `);
     checkCC();
 };
